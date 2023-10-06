2edba7b2a31773e60e6a0f44a525841ad3cc1738
Daniel Bueno <Dbueno3@buffalo.edu>
2023-10-06 13:31:16 -0400

    Removed React files and sticking with html for now

--- rtdemo/src/app/controllers/communications.controller.ts
+++ rtdemo/src/app/controllers/communications.controller.ts
@@ -2,28 +2,9 @@
 import { dependency } from '@foal/core';
 import { EventName, WebsocketContext, WebsocketResponse, WebsocketErrorResponse, WsServer, ValidatePayload } from '@foal/socket.io';
 
-
-interface Message{ 
-  
-  senderId: string;
-  recipientId: string;
-  content: string;
-  timestamp: Date;
-
-}
-
-interface User {
-  id: string;
-  isOnline: boolean;
-  email?: string; // used for notifications
-}
-
 export class CommunicationsController {
   @dependency
   wsServer: WsServer;
-
-  private messageStorage: Message[] = [];
-  private  users: User[] = [];  
 
   echoPayloadSchema = {
     additionalProperties: false,
@@ -32,8 +13,6 @@ export class CommunicationsController {
     required: ['eventName'],
     type: 'object'
   };
-
-
 
   @EventName('/echo')
   @ValidatePayload(controller => controller.echoPayloadSchema)
@@ -51,39 +30,68 @@ export class CommunicationsController {
       payload = {eventName: '/communications/echo'};
     }
     ctx.socket.emit('/communications/echo', payload);
-    this.messageStorage.push({
-      senderId: ctx.socket.id,
-      recipientId: ctx.socket.id,
-      content: payload.eventName,
-      timestamp: new Date()
-    });
-    console.log(this.messageStorage)
 
     return new WebsocketResponse();
   }
 
-    // Updating user's online status
-  @EventName('/update-status')
-  async updateStatus(ctx: WebsocketContext) {
-    const { isOnline } = ctx.payload;
-    const user = this.users.find(u => u.id === ctx.socket.id);
-    if (user) user.isOnline = isOnline;
+  sendMessagePayloadSchema = {
+    additionalProperties: false,
+    properties: {
+      eventName: {type: 'string', minLength: 1},
+      message: {type: 'string', minLength: 1},
+      room: {type: 'string', minLength: 1}
+    },
+    required: ['eventName', 'message', 'room'],
+    type: 'object'
+  };
+
+  @EventName('/send')
+  @ValidatePayload(controller => controller.sendMessagePayloadSchema)
+  async sendMessage(ctx: WebsocketContext) {
+    console.log(`${ctx.controllerName}.${ctx.controllerMethodName} - payload=${JSON.stringify(ctx.payload,null,2)}`);
+    console.log(ctx.socket.id)
+    console.log(ctx.payload)
+    console.log(ctx.eventName)
+    console.log('This is the room', ctx.socket.rooms)
     
+    let payload = ctx.payload;
+    if( !payload ) {
+      payload = {eventName: '/communications/send', message: '', room: ''};
+    }
+
+    const {room, message} = payload
+    console.log('This is the room', room)
+    console.log('This is the message', message)
+
+    // Emitting message to all sockets connected to the room, including sender
+    ctx.socket.to(room).emit('/communications/message', payload);
+    ctx.socket.emit('/communications/message', payload);
+
     return new WebsocketResponse();
   }
 
-  @EventName('get-messages')
-  async getMessages(ctx: WebsocketContext) {
-    // Extract the user's ID and the target recipient ID to fetch the messsage history 
-    const userID = ctx.socket.id;
-    const recipientID = ctx.payload;
 
-    const messages = this.messageStorage.filter(
-      msg => (msg.senderId === userID && msg.recipientId === recipientID) ||
-              (msg.senderId === recipientID && msg.recipientId === userID)
-    )
+
+  @EventName('/broadcast')
+  @ValidatePayload(controller => controller.sendMessagePayloadSchema)
+  async broadcastMessage(ctx: WebsocketContext) {
+    console.log(`${ctx.controllerName}.${ctx.controllerMethodName} - payload=${JSON.stringify(ctx.payload,null,2)}`);
+    console.log(ctx.socket.id)
+    console.log(ctx.payload)
+    console.log(ctx.eventName)
     
-    ctx.socket.emit('get-messages', messages); 
+    let payload = ctx.payload;
+    if( !payload ) {
+      payload = {eventName: '/communications/broadcast', message: ''};
+    }
+
+    const {message} = payload
+    console.log('This is the message', message)
+
+    // Emitting message to all connected sockets
+    this.wsServer.io.emit('/communications/broadcast', payload);
+
+    return new WebsocketResponse();
   }
 
   @EventName('/join-room')
@@ -107,50 +115,6 @@ export class CommunicationsController {
     return new WebsocketResponse();
   }
 
-
-  sendMessagePayloadSchemaV2 = {
-    additionalProperties: false,
-    properties: {
-      eventName: { type: 'string', minLength: 1 },
-      message: { type: 'string', minLength: 1 },
-      recipientId: { type: 'string', minLength: 1 }  // Intended recipient
-    },
-    required: ['eventName', 'message', 'recipientId'],
-    type: 'object'
-  };
-  
-  @EventName('/send-direct')
-  @ValidatePayload(controller => controller.sendMessagePayloadSchema)
-  async sendDirectMessage(ctx: WebsocketContext) {
-    // ... [rest of your method]
-
-    // Storing the message
-    this.messageStorage.push({
-      senderId: ctx.socket.id,
-      recipientId: ctx.payload.recipientId,
-      content: ctx.payload.message,
-      timestamp: new Date()
-    });
-
-    // Emit the message to the recipient
-    ctx.socket.to(ctx.payload.recipientId).emit('/communications/receive-direct', ctx.payload);
-
-    // Check if recipient is offline and send a notification
-    const recipient = this.users.find(u => u.id === ctx.payload.recipientId);
-    if (recipient && !recipient.isOnline) {
-      // Send notification - this might be an email, SMS, or push notification
-      // For example purposes, console log is used here. In real scenarios, use appropriate notification mechanisms.
-      console.log(`User ${recipient.id} is offline. Sending notification to ${recipient.email}`);
-    }
-    
-    return new WebsocketResponse();
-  }
-
-  @EventName('/confrim-reciptent')
-  async confirmRecipient(ctx: WebsocketContext) {
-    const {senderID, messgage} = ctx.payload;
-    ctx.socket.to(senderID).emit('/communications/confirm-reciptent', {message: 'Message received'});
-  }
 
 }
 
